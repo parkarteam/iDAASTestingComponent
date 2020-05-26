7392440307f48b9140292de888bc14add67cca6d
Alan Scott <alscott@redhat.com>
2020-05-26 10:27:01 -0500

    Updates to kafka scripts for delete topics

--- platform-scripts/kafkacmd_topics_delete.sh
+++ platform-scripts/kafkacmd_topics_delete.sh
@@ -2,4 +2,32 @@ _mydir='/Users/alscott/RedHatTech/kafka_2.12-2.4.0.redhat-00005'
 #_mydir='/Users/developer/RedHatTech/kafka_2.12-2.4.0.redhat-00005'
 
 cd $_mydir
-bin/kafka-topics.sh --delete --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic opsMgmt_HL7_RcvdTrans &
+## Operational Topics for Platform
+bin/kafka-topics.sh --delete --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic opsMgmt_PlatformTransactions &
+## HL7
+## Inbound to iDAAS Platform by Message Trigger
+## Facility: MCTN
+## Application: MMS
+bin/kafka-topics.sh --delete --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic MCTN_MMS_ADT &
+## HL7
+## Facility By Application by Message Trigger
+## Facility: MCTN
+## Application: MMS
+bin/kafka-topics.sh --delete --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic MCTN_ADT &
+## HL7
+## Enterprise By Application by Message Trigger
+## Facility: MCTN
+## Application: MMS
+bin/kafka-topics.sh --delete --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic MMS_ADT &
+## HL7
+## Enterprise by Message Trigger
+## Application: MMS
+bin/kafka-topics.sh --delete --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic ENT_ADT &
+
+
+## FHIR
+bin/kafka-topics.sh --delete --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic FHIRSvr_Condition &
+## FHIR
+## Ent.
+bin/kafka-topics.sh --delete --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic Ent_FHIRSvr_Condition &
+
